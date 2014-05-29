845c152d565c164c76c8b5a9aa6ce0f48d27a6b5
pollseed <cb.so.devope+0001@gmail.com>
2014-05-29 23:14:12 +0900

    Initial Commit

---
+++ ToDoSample/TDSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TDSAppDelegate.h
+//  ToDoSample
+//
+//  Created by ogasawara shinnosuke on 2014/05/29.
+//  Copyright (c) 2014年 pollseed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoSample
+//
+//  Created by ogasawara shinnosuke on 2014/05/29.
+//  Copyright (c) 2014年 pollseed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TDSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TDSAppDelegate class]));
+    }
+}
